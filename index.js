6a29c2468f657ed85ceb2d66306d61c4166e0f2c
Adrian Pike <adrian@adrianpike.com>
2017-08-09 13:41:52 -0700

    Clear the timeout 🙈

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const Tls = require('tls');
 
 const Port = Math.floor(Math.random() * PORT_RANGE + PORT_LOWER);
 
+var killer = null;
+
 var cmd = process.argv[2];
 var args = process.argv.slice(3);
 
@@ -39,6 +41,7 @@ const server = Tls.createServer({
   key: Fs.readFileSync(Path.join(__dirname, 'keys', 'key.pem')),
   cert: Fs.readFileSync(Path.join(__dirname, 'keys', 'cert.pem'))
 }, (c) => {
+  clearTimeout(killer);
   console.log('Connection from', c.remoteAddress);
   c.pipe(Child);
 
@@ -59,6 +62,6 @@ server.listen(Port);
 console.log(`Our port is: ${Port}`);
 console.log(`Pipe opened to ${cmd} with args ${args.join(' ')}. Closing in ${WAIT_TIME} seconds unless we get a connection...`);
 
-var killer = setTimeout(function() {
+killer = setTimeout(function() {
   process.exit(0); 
 }, WAIT_TIME * 1000);
